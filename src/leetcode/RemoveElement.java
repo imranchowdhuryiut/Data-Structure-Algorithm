abc7f042a7f337145941377976b521257eecbd23
imran.c@samsung.com <>
2022-08-25 19:16:56 +0600

    Update RemoveElement.java

--- src/leetcode/RemoveElement.java
+++ src/leetcode/RemoveElement.java
@@ -1,5 +1,20 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class RemoveElement {
+
+	public int removeElement(int[] nums, int val) {
+		int result = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == val) {
+				nums[i] = nums.length + 101;
+				result++;
+			}
+		}
+		Arrays.sort(nums);
+		result = nums.length - result;
+		return result;
+	}
 
 }
